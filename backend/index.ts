637f3c5eb5020c83f7f9ae67a522f9be67a81ce5
pedro.darde <pedro.darde@retta.com.br>
2022-04-11 14:24:21 -0300

    feat/ created user table and config to express server

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,12 @@
-import express from 'express'
-import { router } from './routes';
-import cors from 'cors'
+import express from "express";
+import { router } from "./routes";
+import cors from "cors";
 const app = express();
 
-app.use(router)
-app.use(express.json())
+app.use(router);
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.API_PORT || 3335, () => {
+  console.log("api started");
+});
--- backend/src/database/migrations/1649381345157-Users.ts
+++ backend/src/database/migrations/1649381345157-Users.ts
@@ -1,17 +1,61 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Users1649381345157 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "integer",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+            unsigned: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "last_name",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "birth_date",
+            type: "date",
+            isNullable: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "ra",
+            type: "varchar",
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: "document",
+            type: "varchar",
+            isNullable: false,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table(
-            {
-                name: 'users',
-                columns: []
-            }
-        ))
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
 }
