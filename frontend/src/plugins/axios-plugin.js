46fc2a6de8db52b778278c45277291f68d7f9f68
pedro.darde <pedro.darde@retta.com.br>
2022-04-19 10:16:57 -0300

    feat/ add timeout to axios request

--- frontend/src/plugins/axios-plugin.js
+++ frontend/src/plugins/axios-plugin.js
@@ -2,7 +2,11 @@ import axios from "axios";
 class AxiosPlugin {
   #axiosClient;
   constructor() {
-    this.#axiosClient = axios.create({ baseURL: "http://localhost:3337/api" });
+    this.#axiosClient = axios.create({
+      baseURL: "http://localhost:3337/api",
+      timeout: 15000,
+      timeoutErrorMessage: "Tempo limite de espera excedido",
+    });
   }
 
   get(modelName, query) {
